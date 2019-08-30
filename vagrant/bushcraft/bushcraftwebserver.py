f01c2ae66d9d2b79c2bf4f9b3bd09ffcdabe044f
Walker, Silas (sw099y) <sw099y@att.com>
2019-08-30 13:23:12 -0700

    Added screens for when user has no items in pack and/or in gear

--- vagrant/bushcraft/bushcraftwebserver.py
+++ vagrant/bushcraft/bushcraftwebserver.py
@@ -51,8 +51,6 @@ user1 = {'name': 'Silas', 'id': '1'}
 # items = [ {'name':'Cheese Pizza', 'description':'made with fresh cheese', 'price':'$5.99','course' :'Entree', 'id':'1'}, {'name':'Chocolate Cake','description':'made with Dutch Chocolate', 'price':'$3.99', 'course':'Dessert','id':'2'},{'name':'Caesar Salad', 'description':'with fresh organic vegetables','price':'$5.99', 'course':'Entree','id':'3'},{'name':'Iced Tea', 'description':'with lemon','price':'$.99', 'course':'Beverage','id':'4'},{'name':'Spinach Dip', 'description':'creamy dip with fresh spinach','price':'$1.99', 'course':'Appetizer','id':'5'} ]
 item1 =  {'name':'Silky Saw','description':'good saw','price':'$45','weight' :'8'}
 
-# manualID = login_session.get('manualID')
-
 # items = []
 
 @app.route('/')
